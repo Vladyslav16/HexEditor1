5286e2c7b9bb3d87107d8eda0d799a397a2d0166
Vladyslav <118131450+Vladyslav16@users.noreply.github.com>
2025-08-30 14:49:00 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,69 @@
+import pdb
+
+def chunks(data, n):
+    return [data[i:i+n] for i in range(0, len(data), n)]
+
+def one_byte_to_ascii(b):
+    return chr(b) if 0x1F < b < 0x80 else '.'
+
+def bytes_to_ascii(data):
+    return ''.join(one_byte_to_ascii(b) for b in data)
+
+data = list(range(0x61, 0x81))
+
+formats = {
+    400: [4, [ ['', ''] ] + [
+            ['{:08X}: ' + '{:02X} '*i + '   '*(4-i) + '  ', '{}'*i] for i in range(1, 5)
+            ]
+            ],
+
+    700: [8, [ ['', ''] ] + [
+            ['{:08X}: ' + '{:02X} '*i + '   '*(8-i) + '  ', '{}'*i] for i in range(1, 9)
+            ]
+            ],
+
+    1000: [16, [ ['', ''] ] + [
+            ['{:08X}: ' + '{:02X} '*i + '   '*(16-i) + '  ', '{}'*i] for i in range(1, 17)
+            ]
+            ],
+
+    1300: [24, [ ['', ''] ] + [
+            ['{:08X}: ' + '{:02X} '*i + '   '*(24-i) + '  ', '{}'*i] for i in range(1, 25)
+            ]
+            ],
+
+    1600: [32, [ ['', ''] ] + [
+            ['{:08X}: ' + '{:02X} '*i + '   '*(24-i) + '  ', '{}'*i] for i in range(1, 33)
+            ]
+            ],
+    }
+
+def hex_print(data, winW):
+        res = ''
+        bts = 0
+        fmts = []
+        fmt_key = sorted(formats.keys())
+        for maxw in fmt_key:
+            if winW < maxw:
+               bts, fmts = formats[maxw]
+               break
+        else:
+            bts, fmts = formats[fmt_key[-1]]
+
+        addr = 0
+        while addr < len(data):
+            tail = len(data) - addr
+            index = tail if tail < len(fmts) else -1
+            fmt_head, fmt_tail = fmts[index]
+            line = ''.join([fmt_head.format(addr, *data[addr:addr+bts], *[ 0 ]*32),
+                            fmt_tail.format(*list(map(one_byte_to_ascii, data[addr:addr+bts])), *[ 0 ]*32)])
+            sep_index = 10+3*bts//2
+            line = line[:sep_index] + '| ' + line[sep_index:]
+            res += line + '\n'
+            addr += bts
+
+        return res
+
+if __name__ == "__main__":
+    print(hex_print([ ord(c) for c in 'al;weoiurya,kjsdhvfauuweyrdfgfdgdfhdjfgjfyhhhu' ] + [ 0, 5, 8 ], 440))
+
