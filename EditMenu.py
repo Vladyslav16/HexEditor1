c3aed0f7318670bbcb40694f320289ec4f50e531
Vladyslav16 <zarapin.vlad777@gmail.com>
2026-02-07 16:15:30 +0300

    add gitignore

---
+++ EditMenu.py
@@ -0,0 +1,446 @@
+import tkinter as tk
+from window import *
+from EditorContext import *
+from tkinter import messagebox
+
+import pdb
+
+class EditMenu:
+    HEX = 'hex'
+    ASCII = 'ascii'
+    
+    def __init__(self, editor):
+        self.editor = editor
+        self.root = editor.root
+        self.file_data = editor.file_data
+        self.hex_text_widget = editor.hex_text_widget
+        self.hex_format = editor.hex_format
+        
+        self.mode = tk.StringVar(value=EditMenu.ASCII)
+        self.scope_mode = tk.StringVar(value="cursor")
+        self.matchCase_check = tk.BooleanVar(value=False)
+        self.wholeWord_check = tk.BooleanVar(value=False)
+        self.wrapAround_check = tk.BooleanVar(value=True)
+        
+        self.last_search_pos = None
+        self._last_query = None
+        self._last_mode = None
+        self._last_direction = "down"
+        self._last_dialog = None
+
+        self.build_search_dialog()
+        self.build_replace_dialog()
+        
+    def on_open_file(self, file_data):
+        self.file_data = file_data
+        self.last_search_pos = None
+
+    def show_search_dialog(self):
+        dialog = self.search_dialog
+        self._last_dialog = dialog.dialog_type
+        self.show_dialog(dialog)
+        
+    def show_replace_dialog(self):
+        dialog = self.replace_dialog
+        self._last_dialog = dialog.dialog_type
+        self.show_dialog(dialog)
+
+    def show_find_again(self):
+        if self._last_dialog:
+            print(self._last_dialog)
+            if self._last_dialog == "search": dialog = self.search_dialog
+            elif self._last_dialog == "replace": dialog = self.replace_dialog
+            else: return
+            self.find_again(dialog)
+        else: return
+        
+    def show_dialog(self, dialog):
+##        dialog.ascii_entry
+##        dialog.hex_entry
+##        dialog.whole_word_checkbox
+
+        self.get_selection(dialog)
+        self.on_mode_change(dialog)
+
+        dialog.deiconify()
+        dialog.lift()
+        dialog.focus_set()
+        dialog.grab_set()   
+                  
+    def build_search_dialog(self):
+        self.search_dialog = tk.Toplevel(self.root)
+
+        parent = self.search_dialog
+        parent.title("Search Dialog")
+        parent.resizable(False, False)
+        parent.transient(self.root)
+        parent.withdraw()
+
+## Set variables
+        parent.dialog_type = "search"
+        parent.string_ascii = tk.StringVar()
+        parent.string_hex = tk.StringVar()
+        
+## Frame Find
+        find_frame = tk.LabelFrame(
+            parent,
+            text="Find",
+            padx=5,
+            pady=5
+        )
+        find_frame.grid(row=0, column=0, columnspan=2, sticky="wens", padx=5, pady=5)
+        
+        tk.Radiobutton(find_frame, text = "Text string", variable=self.mode, value=EditMenu.ASCII, command=lambda dialog=parent: self.on_mode_change(dialog)
+                       ).grid(row=0, column=0, sticky='w', padx=0, pady=0)
+        tk.Radiobutton(find_frame, text = "Hex string", variable=self.mode, value=EditMenu.HEX, command=lambda dialog=parent: self.on_mode_change(dialog)
+                       ).grid(row=2, column=0, sticky='w', padx=0, pady=0)
+
+        tk.Button(find_frame, text="Text->Hex",  width=8, height=1, command=lambda dialog=parent: self.text_to_hex(dialog)
+                  ).grid(row=0, column=1, columnspan=2, sticky='we', padx=1, pady=1)
+        tk.Button(find_frame, text="Hex->Text", width=8, height=1, command=lambda dialog=parent: self.hex_to_text(dialog)
+                  ).grid(row=2, column=1, columnspan=2, sticky='we', padx=1, pady=1)
+        
+        parent.ascii_entry = tk.Entry(find_frame, textvariable=parent.string_ascii, width=50)
+        parent.ascii_entry.grid(row=1, column=0, columnspan=3, sticky="we", padx=0, pady=0)
+        
+        parent.hex_entry = tk.Entry(find_frame, textvariable=parent.string_hex)
+        parent.hex_entry.grid(row=3, column=0, columnspan=3, sticky="we", padx=0, pady=0)
+        
+## Frame Options
+        opt_frame = tk.LabelFrame(
+                    parent,
+                    text="Options",
+                    padx=5,
+                    pady=5
+                )
+        opt_frame.grid(row=1, column=0, sticky="wens", padx=5, pady=5)
+
+        parent.match_case_checkbox = tk.Checkbutton(opt_frame, text="Match case", variable=self.matchCase_check)
+        parent.match_case_checkbox.grid(row=0, column=1, sticky="w")
+        parent.whole_word_checkbox = tk.Checkbutton(opt_frame, text="Whole word", variable=self.wholeWord_check)
+        parent.whole_word_checkbox.grid(row=1, column=1, sticky="w")
+        parent.wrap_around_checkbox = tk.Checkbutton(opt_frame, text="Wrap around", variable=self.wrapAround_check)
+        parent.wrap_around_checkbox.grid(row=2, column=1, sticky="w")
+        
+## Frame Scope from
+        scope_frame = tk.LabelFrame(
+            parent,
+            text="Scope from",
+            padx=5,
+            pady=5
+        )
+        scope_frame.grid(row=1, column=1, sticky="wens", padx=5, pady=5)
+        
+        tk.Radiobutton(scope_frame, text="Cursor", variable=self.scope_mode, value="cursor"
+                       ).grid(row=0, column=0, sticky="w")
+        tk.Radiobutton(scope_frame, text="Begin", variable=self.scope_mode, value="begin"
+                       ).grid(row=1, column=0, sticky="w")
+## Frame Buttons
+        buttons_frame = tk.LabelFrame(
+            parent,
+            text=" ",
+            padx=5,
+            pady=5
+        )
+        buttons_frame.grid(row=0, column=2, rowspan=2, sticky='wens', padx=5, pady=5)
+
+        tk.Button(buttons_frame, text="Close", command=lambda dialog=parent: self.hide_dialog(dialog)
+                  ).grid(row=0, column=2, sticky="we", padx=5, pady=5)
+        tk.Button(buttons_frame, text="FindDown", command=lambda dialog=parent: self.next_match("down", dialog)
+                  ).grid(row=1, column=2, sticky="we", padx=5, pady=5)
+        tk.Button(buttons_frame, text="FindUp", command=lambda dialog=parent: self.next_match("up", dialog)
+                  ).grid(row=2, column=2, sticky="we", padx=5, pady=5)
+
+        self.search_dialog.protocol(
+            "WM_DELETE_WINDOW",
+            lambda dialog=self.search_dialog: self.hide_dialog(dialog)
+        )
+
+    def build_replace_dialog(self):
+        self.replace_dialog = tk.Toplevel(self.root)
+
+        parent = self.replace_dialog
+        parent.title("Replace Dialog")
+        parent.resizable(False, False)
+        parent.transient(self.root)
+        parent.withdraw()
+
+## Set variables
+        parent.dialog_type = "replace"
+        parent.string_ascii = tk.StringVar()
+        parent.string_hex = tk.StringVar()
+        
+## Frame Find+Replace
+        find_frame = tk.LabelFrame(
+            parent,
+            text="Find",
+            padx=5,
+            pady=5
+        )
+        find_frame.grid(row=0, column=0, columnspan=2, sticky="wens", padx=5, pady=5)
+        
+        tk.Radiobutton(find_frame, text = "Text string", variable=self.mode, value=EditMenu.ASCII, command=lambda dialog=parent: self.on_mode_change(dialog)
+                       ).grid(row=0, column=0, sticky='w', padx=0, pady=0)
+        tk.Radiobutton(find_frame, text = "Hex string", variable=self.mode, value=EditMenu.HEX, command=lambda dialog=parent: self.on_mode_change(dialog)
+                       ).grid(row=2, column=0, sticky='w', padx=0, pady=0)
+
+        tk.Button(find_frame, text="Text->Hex",  width=8, height=1, command=lambda dialog=parent: self.text_to_hex(dialog)
+                  ).grid(row=0, column=1, columnspan=2, sticky='we', padx=1, pady=1)
+        tk.Button(find_frame, text="Hex->Text", width=8, height=1, command=lambda dialog=parent: self.hex_to_text(dialog)
+                  ).grid(row=2, column=1, columnspan=2, sticky='we', padx=1, pady=1)
+        
+        parent.ascii_entry = tk.Entry(find_frame, textvariable=parent.string_ascii)
+        parent.ascii_entry.grid(row=1, column=0, columnspan=3, sticky="we", padx=0, pady=0)
+        
+        parent.hex_entry = tk.Entry(find_frame, textvariable=parent.string_hex)
+        parent.hex_entry.grid(row=3, column=0, columnspan=3, sticky="we", padx=0, pady=0)
+
+        tk.Label(find_frame, text="Replace with:").grid(row=4, column=0, columnspan=3, sticky="w", padx=0, pady=0)
+
+        parent.replace_entry = tk.Entry(find_frame, textvariable=parent.string_ascii, width=50)
+        parent.replace_entry.grid(row=5, column=0, columnspan=3, sticky="we", padx=0, pady=0)
+        
+## Frame Options
+        opt_frame = tk.LabelFrame(
+                    parent,
+                    text="Options",
+                    padx=5,
+                    pady=5
+                )
+        opt_frame.grid(row=1, column=0, sticky="wens", padx=5, pady=5)
+
+        parent.match_case_checkbox = tk.Checkbutton(opt_frame, text="Match case", variable=self.matchCase_check)
+        parent.match_case_checkbox.grid(row=0, column=1, sticky="w")
+        parent.whole_word_checkbox = tk.Checkbutton(opt_frame, text="Whole word", variable=self.wholeWord_check)
+        parent.whole_word_checkbox.grid(row=1, column=1, sticky="w")
+        parent.wrap_around_checkbox = tk.Checkbutton(opt_frame, text="Wrap around", variable=self.wrapAround_check)
+        parent.wrap_around_checkbox.grid(row=2, column=1, sticky="w")
+        
+## Frame Scope from
+        scope_frame = tk.LabelFrame(
+            parent,
+            text="Scope from",
+            padx=5,
+            pady=5
+        )
+        scope_frame.grid(row=1, column=1, sticky="wens", padx=5, pady=5)
+        
+        tk.Radiobutton(scope_frame, text="Cursor", variable=self.scope_mode, value="cursor"
+                       ).grid(row=0, column=0, sticky="w")
+        tk.Radiobutton(scope_frame, text="Begin", variable=self.scope_mode, value="begin"
+                       ).grid(row=1, column=0, sticky="w")
+## Frame Buttons
+        buttons_frame = tk.LabelFrame(
+            parent,
+            text=" ",
+            padx=5,
+            pady=5
+        )
+        buttons_frame.grid(row=0, column=2, rowspan=2, sticky='wens', padx=5, pady=5)
+
+        tk.Button(buttons_frame, text="Close", command=lambda dialog=parent: self.hide_dialog(dialog)
+                  ).grid(row=0, column=2, sticky="we", padx=5, pady=5)
+        tk.Button(buttons_frame, text="Replace+FindDown", command=lambda dialog=parent: self.next_match("down", dialog)
+                  ).grid(row=1, column=2, sticky="we", padx=5, pady=5)
+        tk.Button(buttons_frame, text="Replace+FindUp", command=lambda dialog=parent: self.next_match("up", dialog)
+                  ).grid(row=2, column=2, sticky="we", padx=5, pady=5)
+##        TODO:
+##        Replace + ALL
+        tk.Button(buttons_frame, text="ReplaceAll", command=lambda dialog=parent: self.next_match("up", dialog)
+                  ).grid(row=3, column=2, sticky="we", padx=5, pady=5)
+        
+        self.replace_dialog.protocol(
+            "WM_DELETE_WINDOW",
+            lambda dialog=self.replace_dialog: self.hide_dialog(dialog)
+        )
+
+    def get_search_options(self):
+        class Options:
+            def __init__(self, dialog:EditMenu):
+                self.case  = dialog.matchCase_check.get()
+                self.word  = dialog.wholeWord_check.get()
+                self.wrap  = dialog.wrapAround_check.get()
+                self.scope = dialog.scope_mode.get()
+                self.mode  = dialog.mode.get()
+        return Options(self)
+        
+    def on_mode_change(self, dialog):
+        if self.mode.get() == EditMenu.ASCII:
+            dialog.ascii_entry.config(state="normal")
+            dialog.whole_word_checkbox.config(state="normal")
+            dialog.hex_entry.config(state="disabled")
+            dialog.ascii_entry.focus_set()
+            dialog.ascii_entry.icursor(tk.END)
+        else:
+            self.wholeWord_check.set(False)
+            dialog.whole_word_checkbox.config(state="disabled")
+            dialog.ascii_entry.config(state="disabled")
+            dialog.hex_entry.config(state="normal")
+            dialog.hex_entry.focus_set()
+            dialog.hex_entry.icursor(tk.END)
+
+    def text_to_hex(self, dialog):
+        text = dialog.string_ascii.get()
+        if not text:
+            return
+
+        # ASCII → HEX
+        hex_str = " ".join(f"{ord(c):02X}" for c in text)
+
+        self.mode.set(EditMenu.HEX)
+        self.on_mode_change(dialog)
+
+        dialog.string_hex.set(hex_str)
+        dialog.hex_entry.focus_set()
+        dialog.hex_entry.icursor(tk.END)
+
+    def hex_to_text(self, dialog):
+        _hex = dialog.string_hex.get().strip()
+        if not _hex:
+            return
+
+        # HEX → ASCII
+        data = bytes.fromhex(_hex)
+        text_str = ''.join(
+            chr(b) if 0x20 <= b <= 0x7E else '.'
+            for b in data
+            )
+
+        self.mode.set(EditMenu.ASCII)
+        self.on_mode_change(dialog)
+
+        dialog.string_ascii.set(text_str)
+        dialog.ascii_entry.focus_set()
+        dialog.ascii_entry.icursor(tk.END)
+        
+    def hide_dialog(self, dialog: tk.Toplevel):
+        dialog.grab_release()
+        dialog.withdraw()
+
+    def get_selection(self, dialog):
+        try:
+            selection_begin = self.hex_text_widget.index(tk.SEL_FIRST)
+            selection_end = self.hex_text_widget.index(tk.SEL_LAST)
+        except tk.TclError:
+            return "", EditMenu.ASCII
+        
+        row1, col1 = map(int, selection_begin.split('.'))
+        byte_first = self.hex_format.position_to_byte(row1, col1)
+        row2, col2 = map(int, selection_end.split('.'))
+        byte_last = self.hex_format.position_to_byte(row2, col2)
+
+        self.mode.set((EditMenu.HEX, EditMenu.ASCII)[col1 >= self.hex_format.tail_pos])
+        self.on_mode_change(dialog)
+        
+        data = self.file_data[byte_first:byte_last]
+
+        if not data: return "", self.mode.get()
+
+        if self.mode.get() == EditMenu.HEX:
+            selected_str = bytes_to_hex(data)
+            dialog.string_hex.set(selected_str)
+            print('Selected hex:', selected_str)
+        else: 
+            selected_str = bytes_to_str(data)
+            dialog.string_ascii.set(selected_str)
+            print('Selected text:', selected_str)
+
+        return selected_str, self.mode.get()        
+        
+    def search_query(self, query, mode):
+        if not query:
+            return None
+        query = query.strip()
+        if mode == EditMenu.HEX:
+            query = query.replace(" ", "")
+            if len(query) % 2 != 0:
+                return None
+            return bytearray.fromhex(query)
+
+        return bytearray(query, "ascii")
+
+    def next_match(self, direction, dialog):
+        self._last_direction = direction
+        options = self.get_search_options()
+        
+        mode = self.mode.get()
+        query = dialog.string_ascii.get() if mode == EditMenu.ASCII else dialog.string_hex.get()
+        
+        if not query:
+            return
+        
+        self._last_query = query
+        self._last_mode  = mode
+
+        search_bytes = self.search_query(query, mode)
+        
+        if not search_bytes or not self.file_data:
+            return
+
+        print('Last search pos:', self.last_search_pos)
+        
+        data_len = len(self.file_data)
+        search_bytes_len = len(search_bytes)
+
+        if self.last_search_pos is None:
+            index = self.hex_text_widget.index(tk.SEL_FIRST)
+            row, col = map(int, index.split("."))
+            self.last_search_pos = self.hex_format.position_to_byte(row, col)
+            print('Last search pos changed to:', self.last_search_pos)
+
+        start_index = 0 if options.scope == "begin" else self.last_search_pos
+
+        found = None
+        rng = (range(start_index + 1, data_len - search_bytes_len + 1) if direction == "down"
+        else range(start_index - 1, -1, -1))
+            
+        for i in rng:
+            if self.file_data[i:i+search_bytes_len] == search_bytes:
+## TODO: Option Whole word
+                if options.word:
+                    if not self.check_whole_word(i, search_bytes_len):
+                        continue
+                found = i
+                break
+            
+        if found is None and options.wrap:
+            # До кінця не знайшли. Шукаємо з початку
+            rng = (range(0, start_index) if direction == "down"
+            else range(data_len - search_bytes_len, start_index, -1))
+                
+            for i in rng:
+                if self.file_data[i:i+search_bytes_len] == search_bytes:
+                    found = i
+                    break
+
+        if found is not None:
+            print('Last search pos:', self.last_search_pos, 'found:', found)
+            self.last_search_pos = found
+            # Підсвітка
+            self.hex_format.clear_highlight(self.hex_text_widget, Highlight.SELECTED)
+            bytes_hl.data[Highlight.SELECTED].bytes.clear()
+            
+            for b in range(found, found + search_bytes_len):
+                bytes_hl.data[Highlight.SELECTED].bytes.add(b)
+                
+            self.hex_format.highlight(self.hex_text_widget, Highlight.SELECTED)
+            line, hex_start_col, hex_end_col, ascii_start_col = self.hex_format.byte_coloring_positions(found)
+            self.hex_text_widget.mark_set(tk.INSERT, f"{line}.{hex_start_col}")
+            self.hex_text_widget.see(f"{line}.{hex_start_col}")
+        else:
+            tk.messagebox.showinfo("!", "No matches found.")
+
+    def check_whole_word(self, pos, length):
+        pass
+
+    def find_again(self, dialog):
+        direction = self._last_direction
+        if self._last_query is None or self._last_mode is None:
+            return
+        if self._last_mode == EditMenu.ASCII:
+            dialog.string_ascii.set(self._last_query)
+        else:
+            dialog.string_hex.set(self._last_query)
+
+        self.mode.set(self._last_mode)
+        self.next_match(direction, dialog)
